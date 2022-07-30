1898a84679110e65974ee11001a66e90e1c8c402
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2022-07-30 13:22:33 +0530

    feat: add comfig and head

--- src/Repository.ts
+++ src/Repository.ts
@@ -1,14 +1,16 @@
 import { appendFileSync, existsSync, mkdirSync } from "fs";
+import { stringify as iniStringify } from "ini";
 import { join } from "path";
 
 export class Repository {
     private _baseDir!: string;
 
     public init() {
-        this._baseDir = __dirname;
+        this._baseDir = join(__dirname, "..");
         this._createDirectory("refs/heads")
         this._createDirectory("objects")
-        this._createFile("", "./HEAD")
+        this._createFile("", "HEAD")
+        this._createFile(this._default_config(), "config")
         return this;
     }
 
@@ -38,8 +40,30 @@ export class Repository {
     private _createFile(data: string | Uint8Array, ...args: string[]) {
         this._createDirectory(...args.slice(0, -1));
 
-        appendFileSync(join(...args), data)
+        const filePath = join(this.gitLingDir, ...args);
+        if (existsSync(filePath)) {
+            return filePath
+        }
+        appendFileSync(filePath, data)
 
-        return join(...args)
+        return filePath
+    }
+
+    private _default_config() {
+        const core = {
+            repositoryformatversion: '0',
+            filemode: true,
+            bare: false,
+            logallrefupdates: true,
+            ignorecase: true,
+            precomposeunicode: true
+        }
+        const user = { email: "aniket_chowdhury@hotmail.com", name: "Aniket Biprojit Chowdhury" }
+        const iniString = iniStringify({ core, user }, { whitespace: true }).split("\n").map(elem => {
+            if (elem.startsWith("[")) return elem
+            else elem = "    " + elem
+            return elem
+        }).join('\n')
+        return (iniString);
     }
 }
